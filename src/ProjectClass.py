5b9aac1e8b4774cdbbf3e0aea2a49a88d199fd87
Işın Balcı <isinbalci@Isin-MacBook.local>
2021-11-22 15:00:24 -0600

    isin created

--- src/ProjectClass.py
+++ src/ProjectClass.py
@@ -1,15 +1,16 @@
 class Dynamics(object):
-    def __init__(self, Alist, Blist, Dlist, sigmaWlist):
+    def __init__(self, Alist, Blist, Dlist, dlist, sigmaWlist):
         self.Alist = Alist # list of Ak
         self.Blist = Blist # list of Bk
         self.Dlist = Dlist # list of Dk
+        self.dlist = dlist # list of dk
         self.sigmaWlist = sigmaWlist
-        
+
 class Cost(object):
     def __init__(self, Ru, Rv, muU, muV, sigmaU, sigmaV):
         self.Rulist = Ru
         self.Rvlist = Rv
-        
+
         self.muU = muU
         self.muV = muV
         self.sigmaU = sigmaU
---
+++ src/covsteer_utils.py
@@ -0,0 +1,6 @@
+import numpy as np
+from scipy.linalg import sqrtm
+import cvxpy as cp
+
+def getMatirces():
+    return 
